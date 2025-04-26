80a434eee34dbd2d4c70f032c74e0bf4fb8bb1e9
MusicWizzard <tymon.wolak@gmail.com>
2025-04-26 20:35:26 +0200

    Made a chars_dict_to_sorted_list fumction

--- stats.py
+++ stats.py
@@ -11,3 +11,9 @@ def how_many_chars(text):
         else:
             dictionary[i] += 1
     return dictionary
+def chars_dict_to_sorted_list(char_dict):
+    empty_list = []
+    for i, u in char_dict.items():
+        empty_list.append({"char": i, "num": u})
+    empty_list.sort(reverse=True, key=lambda x:x["num"])
+    return empty_list
