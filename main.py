6911327c90439071760d1d3b29a19c506dd3588a
MusicWizzard <tymon.wolak@gmail.com>
2025-04-26 20:21:15 +0200

    imported how_many_chars along side word_count and used them in the main function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, how_many_chars
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -8,6 +8,8 @@ def main():
     print(x)
     y = word_count(x)
     print(f"{y} words found in the document")
+    z = how_many_chars(x)
+    print(z)
 
 if __name__ == "__main__":
     main()
