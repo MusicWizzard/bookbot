439dfc70a08591592e759be404b45b55fbb629f8
MusicWizzard <tymon.wolak@gmail.com>
2025-04-26 20:19:54 +0200

    Made a how_many_chars function

--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@ def word_count(text):
     string = text.split()
     words = len(string)
     return words
+def how_many_chars(text):
+    ac_text = text.lower()
+    dictionary = {}
+    for i in ac_text:
+        if i not in dictionary:
+            dictionary[i] = 1
+        else:
+            dictionary[i] += 1
+    return dictionary
