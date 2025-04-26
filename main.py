e232aa221ea3cb3417896f23c0949aa2051a1cf6
MusicWizzard <tymon.wolak@gmail.com>
2025-04-26 20:07:15 +0200

    The function word_count has been transfered from main.py to status.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from stats import word_count
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -7,9 +8,6 @@ def main():
     print(x)
     y = word_count(x)
     print(f"{y} words found in the document")
-def word_count(text):
-    string = text.split()
-    words = len(string)
-    return words
+
 if __name__ == "__main__":
     main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def word_count(text):
+    string = text.split()
+    words = len(string)
+    return words
